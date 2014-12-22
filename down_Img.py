5ec505b26fab888cb318cbd1498cbc70eff874b8
sagittariusYX <14210240102@fudan.edu.cn>
2014-12-22 21:43:16 +0800

    add zip function

--- down_Img.py
+++ down_Img.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import os, urllib, urlparse
+import zipfile
 from datetime import datetime
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
@@ -15,29 +17,36 @@ end_num = int(raw_input('Input final page :'))
 for i in range(ori_num,end_num):
     SAMPLEURL.append(utils.get_config('urladdr','URLSAMPLE')%i)
 
-# DOWNPATH
-DOWNPATH = utils.get_config('downpath','DOWNDIR')
-if os.path.isdir(DOWNPATH):
+# TARGETDIR
+TARGETDIR = utils.get_config('downpath','DOWNDIR')
+if os.path.isdir(TARGETDIR):
     pass
 else:
-    os.mkdir(DOWNPATH)
+    os.mkdir(TARGETDIR)
 
 
 def get_url_list(url):
     aList = []
     for eachUrl in url:
         soup = BeautifulSoup(urllib.urlopen(eachUrl).read())
-        for item in soup.find_all('img'):
-            aList.append(str(item.get('src',item.get('data-src'))))
+        map(aList.append, [str(item.get('src', item.get('data-src'))) for item in soup("img")])
     return tuple(aList)
 
 def download_pic(url):
     img_name = urlparse.urlparse(url)[2].split('/')[4]
     try:
-        urllib.urlretrieve(url, os.path.join(DOWNPATH,img_name))
+        urllib.urlretrieve(url, os.path.join(TARGETDIR,img_name))
         LOG.info(img_name + ' ok!')
     except:
         LOG.error('error: ' + img_name + ' failed!')
+
+def adddirfile(dir):
+    f = zipfile.ZipFile('/Users/yangxiao/Documents/python_py/example.zip','w',zipfile.ZIP_DEFLATED)
+    for dirpath, dirnames, filenames in os.walk(dir):
+        for filename in filenames:
+            f.write(os.path.join(TARGETDIR, filename))
+    f.close()
+    print 'zip finished !'
 
 def main():
     # import pdb
@@ -46,11 +55,12 @@ def main():
     urlList = get_url_list(SAMPLEURL)
     start_time = datetime.now()
 
-    # pool = Pool(len(urlList))
-    pool = Pool(8)
+    pool = Pool(len(urlList))
     pool.map(download_pic,urlList)
     pool.close()
     pool.join()
+
+    adddirfile(TARGETDIR)
 
     end_time = datetime.now()
     # print Finished time
