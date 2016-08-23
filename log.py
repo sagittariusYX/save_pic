77673d4d5569df229bc95168e44153e47ef0de59
sagittariusYX <ssdut.xiao@gmail.com>
2016-08-23 23:30:47 +0800

    recover files

---
+++ log.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+logging handler
+"""
+import logging
+import utils
+
+LOGFILENAME = utils.get_config('log', 'LOG_FILE_NAME')
+
+
+def get_logger():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=LOGFILENAME,
+                        filemode='a')
+    return logging
---
+++ utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from __future__ import with_statement
+import ConfigParser
+
+CONF_FILE = 'down_img.conf'
+
+
+def get_config(block, key):
+
+    # get parser
+    config = ConfigParser.ConfigParser()
+
+    # open config file
+    with open(CONF_FILE, 'r') as conf_file:
+        config.readfp(conf_file)
+
+    # get result
+    result = config.get(block, key)
+
+    return result
